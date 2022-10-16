a0ca3ec3b08a7be08dd9d829f2d8e7906d618b63
Gataar <callmeodd33@gmail.com>
2022-10-15 20:13:07 -0400

    bugfix
    bugfix

--- script.js
+++ script.js
@@ -2,16 +2,31 @@ let gold = 0;
 let begValue = 1;
 let goldClick = 1;
 let goldSecond = 0;
-upgrade1.style.display = "none";
-document.getElementById("money").innerHTML = gold;
+let upgrade1 = document.getElementById("paidUpgrade");
 
-let 
+document.getElementById("money").innerHTML = gold;
+if (gold>5){
+      
+        upgrade1.style.display = "block";
+    
+    }
+    else{
+        upgrade1.style.display = "none";
+      
+    }
+
+
+ 
+
+
+
 
 
 function beg(){
     document.getElementById("money").innerHTML = gold;
     gold = gold + begValue;
     console.log(gold);
+  return;
     
 }
 
@@ -24,13 +39,13 @@ function sleepBegging(){
 }
 
 function paidUpgrade(){
-    let upgrade1 = document.getElementById("paidUpgrade")
-    if (gold>5){
-        upgrade1.style.display = "block";
+  
+  
+  //upgrade1.style.display = "none";
+  //begValue = begValue * 2;
+  //gold = gold - 20;
     
-    }
-    else{
-        upgrade1.style.display = "none";
-    }
+   
+  
     
 }
